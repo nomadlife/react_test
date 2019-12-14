8a5bf693ea9d7c1add0a1baa8b33479d93737ae2
nomadlife_15w <joonwoo3816@gmail.com>
2019-12-14 14:25:36 +0900

    state test

--- src/App.js
+++ src/App.js
@@ -7,11 +7,18 @@ import { render } from '@testing-library/react';
 
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      subject:{title:"WEB!!!", sub:"world wide web"}
+    }
+  }
+
   render() {
   return (
 
     <div className="App">
-      <Subject title="WEB!!" sub="world wide web"></Subject>
+      <Subject title={this.state.subject.title} sub={this.state.subject.sub}></Subject>
       <Subject title="React" sub="for UI"></Subject>
       <TOC></TOC>
       <Content title="HTML" desc="HTML is ... "></Content>
