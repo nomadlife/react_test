606a0138c9f32184fc6628fcd27a3ef6bb84f92f
nomadlife_15w <joonwoo3816@gmail.com>
2019-12-14 15:52:09 +0900

    event test

--- src/App.js
+++ src/App.js
@@ -32,7 +32,14 @@ class App extends Component {
     }
   return (
     <div className="App">
-      <Subject title={this.state.subject.title} sub={this.state.subject.sub}></Subject>
+      {/* <Subject title={this.state.subject.title} sub={this.state.subject.sub}></Subject> */}
+      <header>
+            <h1><a href="/" onClick={function(e){
+              console.log(e);
+              e.preventDefault();
+            }}>{this.state.subject.title}</a></h1>
+            {this.state.subject.sub}
+        </header>
       <Subject title="React" sub="for UI"></Subject>
       <TOC data={this.state.contents}></TOC>
       <Content title={_title} desc={_desc}></Content>
