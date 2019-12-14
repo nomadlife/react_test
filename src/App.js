a287464fcb349352d0981e5a60f381fec0752686
nomadlife_15w <joonwoo3816@gmail.com>
2019-12-14 13:43:33 +0900

    component, props, ~L12

--- src/App.js
+++ src/App.js
@@ -1,26 +1,60 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import { render } from '@testing-library/react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+class Subject extends Component {
+  render(){
+    return (
+
+      <header>
+          <h1>{this.props.title}</h1>
+          {this.props.sub}
       </header>
+    );
+  }
+}
+
+class TOC extends Component {
+  render(){
+    return(
+      <nav>
+        <ul>
+            <li><a href="1.html">HTML</a></li>
+            <li><a href="2.html">CSS</a></li>
+            <li><a href="3.html">Javascript</a></li>
+        </ul>
+    </nav>
+    )
+  }
+}
+
+class Content extends Component {
+  render(){
+    return(
+      <article>
+        <h2>{this.props.title}</h2>
+        {this.props.desc}
+        
+    </article>
+    )
+  }
+}
+
+class App extends Component {
+  render() {
+  return (
+
+    <div className="App">
+      <Subject title="WEB!!" sub="world wide web"></Subject>
+      <Subject title="React" sub="for UI"></Subject>
+      <TOC></TOC>
+      <Content title="HTML" desc="HTML is ... "></Content>
     </div>
   );
+    
+  }
 }
+
+
 
 export default App;
